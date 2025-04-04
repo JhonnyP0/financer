055ee73aeb6788726747b3450bfa44a25655989c
Jan <jaspachulski@gmail.com>
2025-04-04 02:08:00 +0200

    register, login added

--- financer.py
+++ financer.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.validators import InputRequired,Length, EqualTo
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import mysql.connector
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config('SECRET_KEY')= os.getenv('API_KEY')
+app.config['SECRET_KEY']= os.getenv('API_KEY')
 
 #flask login config
 login_manager = LoginManager()
@@ -29,14 +31,31 @@ def db_connect():
 
 #user class flask login
 class User(UserMixin):
-    def __init__(self,id,username):
+    def __init__(self,id,username,password):
         self.id=id
         self.username=username
+        self.password=password
+
+
+#login form
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit=SubmitField('Log In')
+#register form
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(),Length(min=5,max=10)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6)])
+    confirm_password=PasswordField('Repeat Password', validators=[InputRequired(), EqualTo('password', message='passwords must be the same')])
+    email = EmailField('E-mail', validators=[InputRequired()])
+    submit=SubmitField('Register')
 
 @login_manager.user_loader
 def userload(user_id):
     conn = db_connect()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
 
     cursor.execute("select * from users where id = %s", (user_id,))
     user= cursor.fetchone()
@@ -45,38 +64,55 @@ def userload(user_id):
     conn.close()
 
     if user:
-        return User(user['id'], user['username'])
-    else:
-        return None
+        return User(user['id'], user['username'], user['password_hash'])
+    return None
 
 @app.route('/', methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
-
+    form=LoginForm()
+    if form.validate_on_submit():
         conn=db_connect()
-        cursor=conn.cursor()
-
-        cursor.execute("select * from users where username = %s", (username,))
-
-        user=cursor.fetchone()
-
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE username = %s", (form.username.data,))
+        user = cursor.fetchone()
         cursor.close()
         conn.close()
 
-        if user:
-            if check_password_hash(user['password'], password):
-                userlog=User(user['id'],user['username'],user['email'])
-                login_user(userlog)
-                flash('Login successful', 'success')
-                return(redirect(url_for('dashboard')))
-            else:
-                flash('Password incorrect', 'danger')
+        if user and check_password_hash(user['password_hash'], form.password.data):
+            userobj=User(user['id'], user['username'], user['password_hash'])
+            login_user(userobj)
+            flash('loged in','success')
+            return redirect(url_for('dashboard'))
         else:
-            flash('User cannot be found', 'danger')
+            flash('Incorrect credentials', 'danger')
+    return render_template('login.html', form=form)
 
-    return render_template('login.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        password=form.password.data
+        hashed_password=generate_password_hash(password)
+        try:
+            conn=db_connect()
+            cursor=conn.cursor(dictionary=True)
+            cursor.execute("INSERT INTO users (username, password_hash, email) VALUES (%s, %s, %s)", (form.username.data, hashed_password, form.email.data,))
+            print(form.username.data, hashed_password, form.email.data)
+            conn.commit()
+            flash('Register succeded','success')
+            return redirect(url_for('login'))
+        except Exception as e:
+            flash(str(e), 'danger')
+        finally:
+            cursor.close()
+            conn.close()
+
+    return render_template('register.html', form=form)
+
+@app.route('/dashboard', methods=['GET'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050, debug=True)
